92afd073bb7dd6a8464f7f746469a8893bddb07a
Tanner McCoy <tmccoy01@vt.edu>
2021-03-30 12:42:19 -0400

    remove Parser class

--- Libs/DataTools.py
+++ Libs/DataTools.py
@@ -5,93 +5,6 @@ from Libs import GeoTools
 from Libs.constants import RADARS, RADIOS, ERAM_SITES
 
 
-class Parser(object):
-    """Load in necessary data"""
-    # TODO: Refactor this to work more cleanly. Maybe, create a new datatype for the Radar's and Radio's?
-    def __init__(self):
-        self.eram_bounds = collections.defaultdict(list)
-        self.service_volumes = {}
-        self.stars_bounds = {}
-        self.enroute_radars = None
-        self.terminal_radars = None
-
-        self._sv_map = {}
-
-    def _load_service_volumes(self):
-        """Load in all of the service volume information"""
-        self.service_volumes['B'] = self._filter_sv(airspace_class='B')
-        self.service_volumes['C'] = self._filter_sv(airspace_class='C')
-        self.service_volumes['D'] = self._filter_sv(airspace_class='D')
-
-    def _load_radars(self, path):
-        """Load in the radar information"""
-        radar_info = [
-            'Radar_Name',
-            'Radar_ID',
-            'Radar_Lat',
-            'Radar_Lon',
-            'PSR Type',
-            'SSR Type',
-            'Airspace_ID',
-            'Comments'
-        ]
-
-        enroute_df = pd.read_excel(path, sheet_name='En Route Radars')
-        self.enroute_radars = enroute_df[radar_info]
-
-        terminal_df = pd.read_excel(path, sheet_name='Terminal Radars')
-        self.terminal_radars = terminal_df[radar_info]
-
-    def _load_radios(self, path=RADIOS):
-        """Only load in the radio information"""
-        radio_df = pd.read_csv(path)
-        self.radio_info = radio_df[
-            [
-                'Alternate Location\n(Airport)',
-                'RSID',
-                'Latitude\n(Degrees)',
-                'Longitude\n(Degrees)',
-                'Enclosed By SV',
-                'ADS-B/WAM Usage'
-            ]
-        ]
-
-    def load_data(self, eram_path=RADARS, radar_path=RADARS, radio_path=RADIOS):
-        """Load in the ERAM, Radar, and Radio information all at once"""
-        self._load_eram(path=eram_path)
-        self._load_radars(path=radar_path)
-        self._load_radios(path=radio_path)
-        self._load_service_volumes()
-
-    def map_radios(self, artcc_id=ERAM_SITES) -> dict:
-        """Create a dictionary mapping of ADS-B radios to ARTCC region"""
-        assert self.radio_info is not None
-        radio_ids = self.radio_info['RSID'].to_list()
-        radio_svs = [id.split('-')[0] for id in radio_ids]
-        for site in ERAM_SITES:
-            sv_id = self._sv_map[site]
-            radio_idx = [ix for ix, val in enumerate(radio_svs) if sv_id in val]
-
-    @staticmethod
-    def _filter_sv(airspace_class=None, path=RADARS):
-        """Load in the service volumes for a given airspace class"""
-        if airspace_class is None:
-            sv_df = pd.read_excel(path, sheet_name='3rdPartySVs')
-        else:
-            sv_df = pd.read_excel(path, sheet_name=f'Terminal Class{airspace_class}')
-        sv_df = sv_df[
-            [
-                'SV ID',
-                'Arpt_Name',
-                'SV_Lat',
-                'SV_Lon',
-                'SV_Range_NM'
-            ]
-        ]
-
-        return sv_df
-
-
 class SurveillanceSystem(object):
     """Main parent class to carry all data for NAS Surveillance Systems"""
     def __init__(self, sv_path=RADARS, radio_path=RADIOS, radar_path=RADARS):
@@ -146,29 +59,57 @@ class Terminal(SurveillanceSystem):
 
 
 class EnRoute(SurveillanceSystem):
-    """Enroute Service Volume Description"""
-    def __init__(self, path=RADARS):
+    """Enroute Service Volume Description (Includes ERAM info)"""
+    def __init__(self):
+        super().__init__()
+        self.sv_map = {}
         self.__load_bounds()
 
     def radio_map(self, radios=None):
         """Not yet implemented"""
-        pass
 
-    def __load_bounds(self, path):
-        """Load in the ERAM information"""
-        artcc_sites = pd.read_excel(path, sheet_name="EnRoute")
+
+    def __load_bounds(self):
+        """Load in the ERAM bounds"""
+        artcc_sites = pd.read_excel(self._radar_path, sheet_name="EnRoute")
         current_id = None
         for row, id in enumerate(artcc_sites.ARTCC_ID):
             if isinstance(id, str):
                 current_id = id
-                self.eram_bounds[id].append(
+                self.sv_bounds[id].append(
                     [artcc_sites.SV_Lat[row], artcc_sites.SV_Lon[row]]
                 )
             else:
-                self.eram_bounds[current_id].append(
+                self.sv_bounds[current_id].append(
                     [artcc_sites.SV_Lat[row], artcc_sites.SV_Lon[row]]
                 )
 
         # Create a map of ARTCC ID --> Service Volume ID
         artcc_sites = artcc_sites[['ARTCC_ID', 'SV ID']].dropna()
-        self._sv_map = dict(zip(artcc_sites['ARTCC_ID'], artcc_sites['SV ID']))
+        self.sv_map = dict(zip(artcc_sites['ARTCC_ID'], artcc_sites['SV ID']))
+
+
+class Radios(object):
+    """Gather all pertinent information for all radios"""
+    def __init__(self, sv=None):
+        self._sv = sv
+        self.radio_df = None
+        self.__load_radios()
+
+    def __load_radios(self):
+        self.radio_df = pd.read_excel(RADIOS, header=6)
+        if self._sv is not None:
+            self.radio_df = self.radio_df[self.radio_df['Enclosed By SV.1'] == self._sv]
+
+        self.radio_df = self.radio_df[
+            [
+                'Operational Status',
+                'LID\n(GBT/[MRU])',
+                'Facility Location',
+                'RSID',
+                'Latitude\n(Degrees)',
+                'Longitude\n(Degrees)',
+                'Enclosed By SV.1',
+                'ADS-B/WAM Usage'
+            ]
+        ]
--- create_radio_spreadsheet.py
+++ create_radio_spreadsheet.py
@@ -1,11 +1,21 @@
-"""This file is just a simple script to grab all necessary radios needed to recreate at 200NM"""
+import pandas as pd
+import collections
 
-from Libs.constants import RADIOS
-from Libs.DataTools import Parser
+from Libs import DataTools, constants
 
 
 def main():
     """Create a spreadsheet for new radio .ter/.flr file creation"""
-    radios = Parser()
-    radios.load_data()
+    radio_df = pd.read_csv(constants.RADIOS)
+    radio_ids = radio_df['RSID'].to_list()
+    radio_svs = [id.split('-')[0] for id in radio_ids]
 
+    eram_data = DataTools.EnRoute()
+
+    radio_sv_map = collections.defaultdict(list)
+    for site in eram_data.sv_map.keys():
+        radio_sv_map[site]
+
+
+if __name__ == '__main__':
+    main()
