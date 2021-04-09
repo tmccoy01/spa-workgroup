c9f8f95a18ef48784a1fd931f28f3fe004df5bea
Tanner McCoy <tmccoy01@vt.edu>
2021-04-09 10:33:21 -0400

    add requirements.txt

--- create_radio_spreadsheet.py
+++ create_radio_spreadsheet.py
@@ -1,21 +1,5 @@
 import pandas as pd
-import collections
-
-from Libs import DataTools, constants
 
 
 def main():
-    """Create a spreadsheet for new radio .ter/.flr file creation"""
-    radio_df = pd.read_csv(constants.RADIOS)
-    radio_ids = radio_df['RSID'].to_list()
-    radio_svs = [id.split('-')[0] for id in radio_ids]
-
-    eram_data = DataTools.EnRoute()
-
-    radio_sv_map = collections.defaultdict(list)
-    for site in eram_data.sv_map.keys():
-        radio_sv_map[site]
-
-
-if __name__ == '__main__':
-    main()
+    df = pd.DataFrame
